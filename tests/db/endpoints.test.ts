af9904d579636acb4224a8c229927741e9d49a74
Matthew Little <zone117x@gmail.com>
2025-01-07 11:50:43 -0700

    fix: use sequence numbers for redis message IDs
    * fix: use sequence numbers for redis message IDs
    
    * test: add client lib test

--- tests/db/endpoints.test.ts
+++ tests/db/endpoints.test.ts
@@ -1,11 +1,14 @@
 import * as fs from 'node:fs';
 import * as readline from 'node:readline/promises';
 import * as zlib from 'node:zlib';
+import * as assert from 'node:assert/strict';
 import { PgStore } from '../../src/pg/pg-store';
 import { EventObserverServer } from '../../src/event-observer/event-server';
 import { Registry } from 'prom-client';
 import { RedisBroker } from '../../src/redis/redis-broker';
 import { ENV } from '../../src/env';
+import { createClient } from 'redis';
+import { StacksEventStream, StacksEventStreamType } from '../../client/src';
 import { timeout } from '@hirosystems/api-toolkit';
 
 describe('Endpoint tests', () => {
@@ -53,8 +56,6 @@ describe('Endpoint tests', () => {
       }
     }
     rl.close();
-
-    await timeout(10000000);
   });
 
   afterAll(async () => {
@@ -63,7 +64,62 @@ describe('Endpoint tests', () => {
     await redisBroker.close();
   });
 
-  test('1+1', () => {
-    console.log('1+1', 1 + 1);
+  test('stream messages from redis', async () => {
+    const appRedisClient = createClient({
+      url: ENV.REDIS_URL,
+      name: 'salt-n-pepper-server-client-test',
+    });
+    await appRedisClient.connect();
+    const streamKey = ENV.REDIS_STREAM_KEY_PREFIX + 'all';
+
+    const queuedMessageCount = await appRedisClient.xLen(streamKey);
+    expect(queuedMessageCount).toBeGreaterThan(0);
+
+    let lastMsgId = '0';
+    let messagedProcessed = 0;
+    for (let i = 0; i < queuedMessageCount; i++) {
+      const streamMessages = await appRedisClient.xRead(
+        { key: streamKey, id: lastMsgId },
+        { BLOCK: 3000, COUNT: 1 }
+      );
+      assert.ok(streamMessages);
+      expect(streamMessages).toHaveLength(1);
+      expect(streamMessages[0].messages).toHaveLength(1);
+      lastMsgId = streamMessages[0].messages[0].id;
+      messagedProcessed++;
+    }
+    expect(messagedProcessed).toBe(queuedMessageCount);
+    expect(lastMsgId).toBe(`${queuedMessageCount}-0`);
+    await appRedisClient.quit();
+  });
+
+  test('client lib test', async () => {
+    let lastMsgId = '0';
+    const client = new StacksEventStream({
+      redisUrl: ENV.REDIS_URL,
+      eventStreamType: StacksEventStreamType.all,
+      lastMessageId: lastMsgId,
+    });
+    await client.connect({ waitForReady: true });
+    let messagesProcessed = 0;
+    let lastTimestamp = 0;
+    client.start(async (id, timestamp, path, body) => {
+      lastMsgId = id;
+
+      expect(typeof path).toBe('string');
+      expect(path).not.toBe('');
+
+      expect(typeof body).toBe('object');
+      expect(Object.entries(body as object).length).toBeGreaterThan(0);
+
+      expect(parseInt(timestamp)).toBeGreaterThanOrEqual(lastTimestamp);
+      lastTimestamp = parseInt(timestamp);
+
+      messagesProcessed++;
+      await Promise.resolve();
+    });
+    await timeout(500);
+    await client.stop();
+    expect(messagesProcessed).toBeGreaterThan(0);
   });
 });

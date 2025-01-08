97ef44eba4ebcf82940889a3950d33391968ec0c
Matthew Little <zone117x@gmail.com>
2025-01-08 10:11:25 -0700

    test: create stackerdb message ingestion test

--- tests/db/stackerdb-ingestion.test.ts
+++ tests/db/stackerdb-ingestion.test.ts
@@ -8,16 +8,11 @@ import { Registry } from 'prom-client';
 import { RedisBroker } from '../../src/redis/redis-broker';
 import { ENV } from '../../src/env';
 import { createClient } from 'redis';
-import { StacksEventStream, StacksEventStreamType } from '../../client/src';
-import { timeout } from '@hirosystems/api-toolkit';
-import { buildPromServer } from '../../src/prom/prom-server';
-import { FastifyInstance } from 'fastify';
 
-describe('Endpoint tests', () => {
+describe('Stackerdb ingestion tests', () => {
   let db: PgStore;
   let redisBroker: RedisBroker;
   let eventServer: EventObserverServer;
-  let promServer: FastifyInstance;
 
   beforeAll(async () => {
     db = await PgStore.connect();
@@ -40,11 +35,8 @@ describe('Endpoint tests', () => {
     });
     await eventServer.start({ port: 0, host: '127.0.0.1' });
 
-    promServer = await buildPromServer({ registry: promRegistry });
-    await promServer.listen({ host: ENV.OBSERVER_HOST, port: 0 });
-
     // insert stacks-node events dump
-    const payloadDumpFile = './tests/dumps/epoch-3-transition.tsv.gz';
+    const payloadDumpFile = './tests/dumps/stackerdb-sample-events.tsv.gz';
     const rl = readline.createInterface({
       input: fs.createReadStream(payloadDumpFile).pipe(zlib.createGunzip()),
       crlfDelay: Infinity,
@@ -70,30 +62,6 @@ describe('Endpoint tests', () => {
     await eventServer.close();
     await db.close();
     await redisBroker.close();
-    await promServer.close();
-  });
-
-  test('status endpoint check', async () => {
-    const res = await fetch(eventServer.url + '/status');
-    expect(res.status).toBe(200);
-    const body = await res.json();
-    expect(body).toMatchObject({
-      server_version: expect.stringMatching(/^salt-n-pepper v/),
-      status: 'ready',
-    });
-  });
-
-  test('prom server test', async () => {
-    const addrs = promServer.addresses();
-    const promUrl = `http://${addrs[0].address}:${addrs[0].port}/metrics`;
-    const res = await fetch(promUrl);
-    expect(res.status).toBe(200);
-    expect(res.headers.get('content-type')).toMatch(/^text\/plain/);
-    const body = await res.text();
-    expect(body).toMatch(/http_request_duration_seconds_bucket/);
-    const expectedLinePrefix =
-      'http_request_duration_seconds_count{method="POST",route="/new_block",status_code="200"}';
-    expect(body).toMatch(new RegExp(`^${expectedLinePrefix}\\s*\\d+$`, 'm'));
   });
 
   test('stream messages from redis', async () => {
@@ -123,36 +91,5 @@ describe('Endpoint tests', () => {
     expect(messagedProcessed).toBe(queuedMessageCount);
     expect(lastMsgId).toBe(`${queuedMessageCount}-0`);
     await appRedisClient.quit();
-  });
-
-  test('client lib test', async () => {
-    let lastMsgId = '0';
-    const client = new StacksEventStream({
-      redisUrl: ENV.REDIS_URL,
-      eventStreamType: StacksEventStreamType.all,
-      lastMessageId: lastMsgId,
-      redisStreamPrefix: ENV.REDIS_STREAM_KEY_PREFIX,
-    });
-    await client.connect({ waitForReady: true });
-    let messagesProcessed = 0;
-    let lastTimestamp = 0;
-    client.start(async (id, timestamp, path, body) => {
-      lastMsgId = id;
-
-      expect(typeof path).toBe('string');
-      expect(path).not.toBe('');
-
-      expect(typeof body).toBe('object');
-      expect(Object.entries(body as object).length).toBeGreaterThan(0);
-
-      expect(parseInt(timestamp)).toBeGreaterThanOrEqual(lastTimestamp);
-      lastTimestamp = parseInt(timestamp);
-
-      messagesProcessed++;
-      await Promise.resolve();
-    });
-    await timeout(500);
-    await client.stop();
-    expect(messagesProcessed).toBeGreaterThan(0);
   });
 });

69448be6ded794a59a9c8ce030d35bd06c0866ad
Rafa Cárdenas <253999660+rafa-stacks@users.noreply.github.com>
2026-02-10 12:32:54 -0600

    fix(client): add types for attachment and microblock messages (#46)
    * chore: add missing msgs
    
    * chore: use stacks transactions
    
    * ci: install client deps
    
    * fix: build client in dockerfile
    
    * fix: ci client in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,11 @@ COPY . .
 
 RUN apk add --no-cache --virtual .build-deps git
 RUN npm ci && \
+    npm ci --prefix client && \
     npm run build && \
     npm run generate:git-info && \
-    npm prune --production
+    npm prune --production && \
+    npm prune --production --prefix client
 RUN apk del .build-deps
 
-CMD ["node", "./dist/src/index.js"]
+CMD ["node", "./dist/src/index.js", "&&", "node", "./client/dist/src/index.js"]
--- client/src/messages/new-burn-block.ts
+++ client/src/messages/new-burn-block.ts
@@ -1,20 +1,42 @@
-export interface NewBurnBlockMessage {
-  burn_block_hash: string;
-  burn_block_height: number;
+export interface NewBurnBlockRewardRecipient {
+  /** Bitcoin address (b58 encoded). */
+  recipient: string;
   /** Amount in BTC satoshis. */
-  burn_amount: number;
-  reward_recipients: [
-    {
-      /** Bitcoin address (b58 encoded). */
-      recipient: string;
-      /** Amount in BTC satoshis. */
-      amt: number;
-    },
-  ];
+  amt: number;
+}
+
+/** PoX payout event */
+export interface NewBurnBlockPoxTransactionRewardRecipient {
+  recipient: string;
+  amt: number;
+  utxo_idx: number;
+}
+
+/** PoX payout transaction */
+export interface NewBurnBlockPoxTransaction {
+  txid: string;
+  reward_recipients: NewBurnBlockPoxTransactionRewardRecipient[];
+}
+
+export interface NewBurnBlockMessage {
+  /** The hash of the burn block */
+  burn_block_hash: string;
+  /** The height of the burn block */
+  burn_block_height: number;
+  /** The reward recipients */
+  reward_recipients: NewBurnBlockRewardRecipient[];
   /**
    * Array of the Bitcoin addresses that would validly receive PoX commitments during this block.
    * These addresses may not actually receive rewards during this block if the block is faster
    * than miners have an opportunity to commit.
    */
   reward_slot_holders: string[];
+  /** The amount of BTC burned during the burn block */
+  burn_amount: number;
+  /** The consensus hash of the burn block */
+  consensus_hash: string;
+  /** The hash of the parent burn block */
+  parent_burn_block_hash: string;
+  /** The individual transaction information from signers */
+  pox_transactions: NewBurnBlockPoxTransaction[];
 }

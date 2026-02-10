fc96559dc2463ef1de387d66d27a62c7735c1487
Rafa Cardenas <253999660+rafa-stacks@users.noreply.github.com>
2026-02-10 10:59:29 -0600

    chore: use stacks transactions

--- client/src/messages/new-block.ts
+++ client/src/messages/new-block.ts
@@ -1,3 +1,5 @@
+import { ClarityAbi } from '@stacks/transactions';
+
 export enum NewBlockEventType {
   Contract = 'contract_event',
   StxTransfer = 'stx_transfer_event',
@@ -230,80 +232,6 @@ export type NewBlockEvent =
   | NewBlockNftBurnEvent;
 
 export type NewBlockTransactionStatus = 'success' | 'abort_by_response' | 'abort_by_post_condition';
-
-export type ClarityAbiTypeBuffer = { buffer: { length: number } };
-export type ClarityAbiTypeResponse = { response: { ok: ClarityAbiType; error: ClarityAbiType } };
-export type ClarityAbiTypeOptional = { optional: ClarityAbiType };
-export type ClarityAbiTypeTuple = { tuple: { name: string; type: ClarityAbiType }[] };
-export type ClarityAbiTypeList = { list: { type: ClarityAbiType; length: number } };
-
-export type ClarityAbiTypeUInt128 = 'uint128';
-export type ClarityAbiTypeInt128 = 'int128';
-export type ClarityAbiTypeBool = 'bool';
-export type ClarityAbiTypePrincipal = 'principal';
-export type ClarityAbiTypeNone = 'none';
-
-export type ClarityAbiTypePrimitive =
-  | ClarityAbiTypeUInt128
-  | ClarityAbiTypeInt128
-  | ClarityAbiTypeBool
-  | ClarityAbiTypePrincipal
-  | ClarityAbiTypeNone;
-
-export type ClarityAbiType =
-  | ClarityAbiTypePrimitive
-  | ClarityAbiTypeBuffer
-  | ClarityAbiTypeResponse
-  | ClarityAbiTypeOptional
-  | ClarityAbiTypeTuple
-  | ClarityAbiTypeList;
-
-export interface ClarityAbiFunction {
-  name: string;
-  access: 'private' | 'public' | 'read_only';
-  args: {
-    name: string;
-    type: ClarityAbiType;
-  }[];
-  outputs: {
-    type: ClarityAbiType;
-  };
-}
-
-export interface ClarityAbiVariable {
-  name: string;
-  access: 'variable' | 'constant';
-  type: ClarityAbiType;
-}
-
-export interface ClarityAbiMap {
-  name: string;
-  key: {
-    name: string;
-    type: ClarityAbiType;
-  }[];
-  value: {
-    name: string;
-    type: ClarityAbiType;
-  }[];
-}
-
-export interface ClarityAbiTypeFungibleToken {
-  name: string;
-}
-
-export interface ClarityAbiTypeNonFungibleToken {
-  name: string;
-  type: ClarityAbiType;
-}
-
-export interface ClarityAbi {
-  functions: ClarityAbiFunction[];
-  variables: ClarityAbiVariable[];
-  maps: ClarityAbiMap[];
-  fungible_tokens: ClarityAbiTypeFungibleToken[];
-  non_fungible_tokens: ClarityAbiTypeNonFungibleToken[];
-}
 
 export interface NewBlockExecutionCost {
   read_count: number;
